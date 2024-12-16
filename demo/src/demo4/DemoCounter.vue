313711e241fa398cf370b45dc61d6927ef3596a6
tatcher <tatcher@ecwid.com>
2024-12-17 00:18:27 +0400

    added stores

---
+++ demo/src/demo4/DemoCounter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>Demo 4</div>
+  <demo-container>
+    <div class="counter">
+      <counter-button
+          :disabled="counterStore.isDecrementDisabled"
+          @action="counterStore.decrement"
+      >
+        -
+      </counter-button>
+
+      <counter-value :value="counterStore.value"/>
+
+      <counter-button
+          @action="counterStore.increment"
+      >
+        +
+      </counter-button>
+    </div>
+  </demo-container>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "@/demo1/CounterValue.vue";
+import DemoContainer from "@/components/DemoContainer.vue";
+import {useCounterStore} from "./store";
+
+const count = ref(0);
+
+const counterStore = useCounterStore()
+
+</script>
+
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
